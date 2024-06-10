c23c36a1018365e49f7fea6b161c5ca23e65e864
Vili Ripatti <vili.ripatti@successclinic.fi>
2024-06-10 11:59:10 +0300

    tulisko tää läpi nyt?

--- part_1.py
+++ part_1.py
@@ -169,17 +169,249 @@ def find_honi():
 
 
 # https://dmoj.ca/problem/ccc11s1 - English or French
+
+count = int(input())
+text=""
+t_count=0
+s_count=0
+for i in range(count):
+    text += input()
+
+
+for i in text:
+    t_count += i.count("t") + i.count("T")
+    s_count += i.count("s") + i.count("S")
+
+if t_count > s_count:
+    print("English")
+else:
+    print("French")
+
 # https://dmoj.ca/problem/ccc11s2 - Multiple Choice
+
+n = int(input())
+count=0
+student_a = []
+right_a = []
+i=0
+for i in range(2*n):
+    ans = input()
+    if i <= n-1:
+        student_a.append(ans)
+    else:
+        right_a.append(ans)
+j=0
+for j in range(n):
+    if student_a[j-1] == right_a[j-1]:
+        count +=1
+
+print(count)
+
 # https://dmoj.ca/problem/coci12c5p1 - Ljestvica
+
+
+string_full = "AEB|CD"
+string = string_full.split("|")
+C = ["C","F","G"]
+A = ["A","D","E"]
+A_mol = []
+C_dur = []
+for i in string:
+    if i[0] in A:
+        A_mol.append(i)
+    elif i[0] in C:
+        C_dur.append(i)
+
+if len(C_dur) > len(A_mol):
+    print("C-dur")
+elif len(A_mol) > len(C_dur):
+    print("A-mol")
+elif len(C_dur) == len(A_mol):
+    if string_full[-1]=="C":
+        print("C-dur")
+    elif string_full[-1]=="A":
+        print("A-mol")
+
+
+
 # https://dmoj.ca/problem/coci13c3p1 - Rijeci
+
+start = "A"
+k = int(input())
+text = ""
+for i in range(k):
+    text = ""
+    for c in start:
+        if c == "A":
+            text += "B"
+        else:
+            text +="BA"
+    start=text
+A_count = start.count("A")
+B_count = start.count("B")
+print(f"{A_count} {B_count}")
+
+"""
+Tuli runtime erroreita niin pyysin chatgpt:tä tekemään mulle vauhdikkaan version tästä 
+
+"k = int(input())
+A_count = 1
+B_count = 0
+
+for _ in range(k):
+    new_B_count = A_count
+    new_A_count = B_count
+    new_B_count += B_count
+    A_count = new_A_count
+    B_count = new_B_count
+
+print(f"{A_count} {B_count}")"
+
+#Vähä erilainen lähestymistapa mut pirun sliikki kyl
+
+"""
+
 # https://dmoj.ca/problem/coci18c4p1 - Elder
 
+first_wiz = input()
+owner = first_wiz
+n = int(input())
+winners = []
+winners.append(owner)
+for i in range(n):
+    fight = input()
+    fight = fight.split(" ")
+    if fight[0] == owner:
+        owner = fight[0]
+        winners.append(owner)
+    elif fight[1] == owner:
+        owner = fight[0]
+        winners.append(owner)
+    else:
+        pass
+
+wand_holders = len(list(set(winners)))
+print(owner)
+print(wand_holders)
+
 # https://dmoj.ca/problem/ccc20j2 - Epidemiology
+
+P = int(input())
+N = int(input())
+R = int(input())
+
+i=0
+sum=0
+while sum <= P:
+    sum += N*R**i
+    i+=1
+
+print(i-1)
+
 # https://dmoj.ca/problem/coci08c1p2 - Ptice
+
+N = int(input())
+answers = input()
+
+boys = {"Adrian":"ABC","Bruno":"BABC","Goran":"CCAABB"}
+for boy in boys:
+    jj= N // len(boys[boy])
+    ja = N % len(boys[boy])
+    boys[boy] = jj*boys[boy][:N] + boys[boy][:ja]
+    
+results = []
+dict= {"Name":[],"Points":[]}
+for boy in boys:
+    for c in range(len(answers)):
+        if boys[boy][c] == answers[c]:
+            results.append(boy)
+
+    dict["Name"].append(boy)
+    dict["Points"].append(results.count(boy))
+
+print(dict["Points"][dict["Points"].index(max(dict["Points"]))])
+
+for i, x in enumerate(dict["Points"]):
+        if x == max(dict["Points"]):
+            print(dict["Name"][i])
+
+
+
+
 # https://dmoj.ca/problem/ccc02j2 - AmeriCanadian
+
+str = ""
+vowels = "aeiouy"
+while str != "quit!":
+    str = input()
+    if len(str) > 4 and str[-2:] =="or" and str[-3] not in vowels:
+        canadian = str.replace(str[-2:],"our")
+        print(canadian)
+    elif str == "quit!":
+        pass
+    else:
+        print(str)
+        
+
+
 # https://dmoj.ca/problem/ecoo13r1p1 - Take a Number
+
+num = int(input())
+count_serve = 0
+count_take = 0
+
+while True:
+    str = input()
+    if str == "EOF":
+        break
+    
+    if str == "TAKE":
+        count_take += 1
+        num += 1
+        if num > 999:
+            num = 1
+    elif str == "SERVE":
+        count_serve += 1
+    else:
+        print(f"{count_take} {count_take - count_serve} {num}")
+        count_take = 0
+        count_serve = 0
+
+
 # https://dmoj.ca/problem/ecoo15r1p1 - When You Eat Your Smarties
+
+
+for i in range(10):
+    smarties = []
+    while True:
+        smartie = input()
+        if smartie == "end of box":
+            break
+        smarties.append(smartie)
+    
+    d = dict.fromkeys(smarties,0)
+    for val in smarties:
+        d[val] += 1
+
+    total_time = 0
+
+    for candy in d:
+        if candy == "red":
+            red_time = d[candy]*16
+            total_time += red_time
+        else:
+            if d[candy] % 7 == 0:
+                other_time = (d[candy]//7)*13
+                total_time += other_time
+            else:
+                other_time = (d[candy]//7+1)*13
+                total_time += other_time
+
+    print(total_time)
+
 # https://dmoj.ca/problem/ccc19j3 - Cold Compress
+
+
 
 # https://dmoj.ca/problem/ccc07j3 - Deal or No Deal Calculator
 # https://dmoj.ca/problem/coci17c1p1 - Cezar
